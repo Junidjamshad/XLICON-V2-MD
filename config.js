738f42e76512ca9c2dc784bdc4b505e20e48125d
𝙅𝙐𝞜𝞓𝞘𝘿 𝟲𝟴𝟳🪀 <162732210+Junidjamshad@users.noreply.github.com>
2024-04-10 13:35:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['923184070915', '*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*• -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233531910067', 'Abraham Dwamena3', true],
@@ -55,21 +55,21 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*•'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*• TEAM' 
+global.author = '@*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*•' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
+global.igfg = '▢✓ Follow My https://chat.whatsapp.com/HLaHhHH9RedKDSgoBtNzDQ
+global.dygp = 'https://chat.whatsapp.com/HLaHhHH9RedKDSgoBtNzDQ
 global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
 global.fgyt = 'https://youtube.com/@s4salmanyt'
 global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
+global.fglog = '*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*•.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*• LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
